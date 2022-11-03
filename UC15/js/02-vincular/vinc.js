9d2761a77eb9adea91c62d3f063d30335ca54e21
FAGNER GERALDES BRAGA <fagner.fgb@gmail.com>
2022-11-03 20:08:04 -0300

    Atualizando a documentação

---
+++ UC15/js/02-vincular/vinc.js
@@ -0,0 +1,6 @@
+// comentários
+/* comentários */
+
+document.write("Fagner Geraldes Braga");
+document.write("<h2>" + "Fagner Geraldes Braga" + "</h2>");
+document.write(`<h3>Fagner Geraldes Braga</h3>`);
---
+++ UC15/js/03-funcao-simples/funcao.js
@@ -0,0 +1,6 @@
+function simples(){
+    var nome;
+    nome = prompt("Qual é o seu nome ?");
+    alert(`Olá, ${nome}`);
+    //alert("Olá, " + nome);
+}
