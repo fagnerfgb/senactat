e0bd52fe690c5984508543ba4ccc4973025993aa
FAGNER GERALDES BRAGA <fagner.fgb@gmail.com>
2022-11-03 21:13:51 -0300

    Atualizando a documentação

--- UC15/js/03-funcao-simples/funcao.js
+++ UC15/js/03-funcao-simples/funcao.js
@@ -1,4 +1,4 @@
-function simples(){
+function simples() {
     var nome;
     nome = prompt("Qual é o seu nome ?");
     alert(`Olá, ${nome}`);
--- UC15/js/calculadora/calculadora.js
+++ UC15/js/calculadora/calculadora.js
@@ -1,19 +1,19 @@
 var etanol, gasolina;
 function calcular() {
-    gasolina = parseFloat(frmFlex.txtGasolina.value.replace(",","."))
-    etanol = parseFloat(frmFlex.txtEtanol.value.replace(",","."))
-    if (etanol < 0.7 * gasolina){
+    gasolina = parseFloat(frmFlex.txtGasolina.value.replace(",", "."))
+    etanol = parseFloat(frmFlex.txtEtanol.value.replace(",", "."))
+    if (etanol < 0.7 * gasolina) {
         alert("Vai de etanol");
-        document.getElementById("status").src="img/etanol.png";               
+        document.getElementById("status").src = "img/etanol.png";
     }
     else {
         alert("Vai de gasolina");
-        document.getElementById("status").src="img/gasolina.png"; 
+        document.getElementById("status").src = "img/gasolina.png";
     }
 }
-function resetar(){
-    document.getElementById("status").src="img/neutro.png";
-   frmFlex.reset();
+function resetar() {
+    document.getElementById("status").src = "img/neutro.png";
+    frmFlex.reset();
 
 
 
