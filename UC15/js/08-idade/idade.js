34d5cf5f667acde5030b13b17899696cfcfd1b19
FAGNER GERALDES BRAGA <fagner.fgb@gmail.com>
2022-11-10 19:12:43 -0300

    Atualizando a documentação

--- UC15/js/08-idade/idade.js
+++ UC15/js/08-idade/idade.js
@@ -22,31 +22,32 @@ function verificar() {
 				var sexo = masc
 			}
 			var idade = anoatual - Number(anodig.value)
+			res.innerHTML = `<p>A sua idade é: ${idade} anos</p>`
 			if (idade < 11) {
 				if (sexo == masc) {
-					res.innerHTML = `<img src=img/menino.png><p>Você é menino!</p>`
+					res.innerHTML += `<img src=img/menino.png><p>Você é um menino!</p>`
 				} else {
-					res.innerHTML = `<img src=img/menina.png><p>Você é menina!</p>`
+					res.innerHTML += `<img src=img/menina.png><p>Você é uma menina!</p>`
 				}
 			}
 			else if (idade >= 11 && idade < 24) {
 				if (sexo == masc) {
-					res.innerHTML = `<img src=img/jovem_m.png><p>Você é moço!</p>`
+					res.innerHTML += `<img src=img/jovem_m.png><p>Você é um rapaz!</p>`
 				} else {
-					res.innerHTML = `<img src=img/jovem_f.png><p>Você é moça!</p>`
+					res.innerHTML += `<img src=img/jovem_f.png><p>Você é uma moça!</p>`
 				}
 			}
 			else if (idade >= 24 && idade < 64) {
 				if (sexo == masc) {
-					res.innerHTML = `<img src=img/adulto.png><p>Você é adulto!</p>`
+					res.innerHTML += `<img src=img/adulto.png><p>Você é um adulto!</p>`
 				} else {
-					res.innerHTML = `<img src=img/adulta.png><p>Você é adulta!</p>`
+					res.innerHTML += `<img src=img/adulta.png><p>Você é uma adulta!</p>`
 				}
 			} else {
 				if (sexo == masc) {
-					res.innerHTML = `<img src=img/idoso.png><p>Você é idoso!</p>`
+					res.innerHTML += `<img src=img/idoso.png><p>Você é um senhor de respeito!</p>`
 				} else {
-					res.innerHTML = `<img src=img/idosa.png><p>Você é idosa!</p>`
+					res.innerHTML += `<img src=img/idosa.png><p>Você é uma senhora de respeito!</p>`
 				}
 			}
 		}
@@ -59,6 +60,9 @@ function limpar() {
 	for (var i = 0; i < limpaSexo.length; i++) {
 		limpaSexo[i].checked = false
 	}
+	const div = document.querySelector("#resp")
+	div.innerHTML = ""
+	div.innerHTML = `<p>Preencha os dados acima para ver o resultado!</p>`
 	document.querySelector("#tano").focus()
 }
 
